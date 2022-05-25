bda39528335e161b11a92e2ccfd8b34c550a20e4
Fake Developer <97069726+NotADevGuy@users.noreply.github.com>
2022-05-24 20:31:46 -0400

    Moving computers, need to update as much as I can

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 const mineflayer = require('mineflayer')
-const {pathfinder, Movements, goals: { GoalNear } } = require('mineflayer-pathfinder')
+const {pathfinder, Movements, goals } = require('mineflayer-pathfinder')
 const pvp = require('mineflayer-pvp').plugin
+const autoEat = require('mineflayer-auto-eat')
+const armorManager = require('mineflayer-armor-manager')
+const projectile = require(`mineflayer-projectile`)
+const toolPlugin = require('mineflayer-tool').plugin
+
 
 const bossName = "NotACrazyGuy"
 const prefix = 'leebot_'
@@ -13,7 +18,7 @@ let bots = []
 const server = {
     address: 'localhost',
     version: '1.18.2',
-    port: 31312
+    port: 1522
 }
 
 const createBot = () => {
@@ -25,36 +30,57 @@ const createBot = () => {
     })
     botnum = botnum + 1
     bot.loadPlugin(pathfinder)
-    bot.once('spawn', () =>{
-        const mcData = require('minecraft-data')(server.version)
-        const defaultMove = new Movements(bot, mcData)
+    bot.loadPlugin(autoEat)
 
-        bot.on('chat', (username, message) => {
-            if (username === bot.username) {
-                return 0
-            }
-            if (message !== 'come') {
-                return 0
-            }
-            const target = bot.players[bossName]?.entity
-            if (!target) {
-                // bot.chat("I don't see you")
-                return
-            }
-            const {x: playerX, y: playerY, z: playerZ} = target.position
 
-            bot.pathfinder.setMovements(defaultMove)
-            bot.pathfinder.setGoal(new GoalNear(playerX, playerY, playerZ, closeness))
-        })
-
+    // These four commands are used for auto-eating.
+    bot.once("spawn", () => {
+        bot.autoEat.options = {
+            priority: 'foodPoints',
+            startAt: 16,
+            eatingTimeout: 3,
+            bannedFood: ["golden_apple", "enchanted_golden_apple", "rotten_flesh", "poisonous_potato", "spider_eye", "dried_kelp"]
+        }
     })
+
+    bot.on("autoeat_started", () => {
+        console.log("Auto Eat started!")
+    })
+
+    bot.on("autoeat_stopped", () => {
+        console.log("Auto Eat stopped!")
+    })
+
+    bot.on("health", () => {
+        if (bot.food === 20) {
+            bot.autoEat.disable()
+        }
+        else {
+            bot.autoEat.enable()
+        }
+    })
+
+    bot.on('login', () =>{
+        let botSocket = bot._client.socket
+        console.log(`Logged in to ${botSocket.server ? botSocket.server : botSocket._host}`)
+    })
+
+    bot.on('end', () => {
+        console.log(bot.username + " disconnected")
+        setTimeout(createBot(), 5000)
+    })
+
+    bot.on('error', (err) => {
+        console.log(bot.username + ` ${err}`)
+    })
+
 
     return bot
 }
 
 const populate = () => {
     bots.push(createBot())
-    if (botnum <= 7){
+    if (botnum <= 1){
         setTimeout(populate, 500)
     }
 }
